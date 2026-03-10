9bd073685367ca7af3e0318d733a5b8675ec6c4f
GeiziRocha <geizirocha586@gmail.com>
2026-03-10 16:02:25 -0300

    aula19

--- python/funcoes.py
+++ python/funcoes.py
@@ -5,3 +5,18 @@ def saudacao(nome, estado):
     print(f"Olá {nome}, seja bem-vindo! Muito legal ver alguém do {estado}\n")
 
 saudacao(nome, estado)
+
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+def soma(a, b):
+    return a + b
+
+# def potencia(a, b):
+#    return a ** b
+
+resultado = soma(a, b)
+print(f"O resultado da soma de {a} com {b} é de: {resultado}")
+
+#resultado = potencia(a, b)
+#print(f"O resultado da potência de {a} com {b} é de: {resultado}")
